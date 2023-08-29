3988a6e37fdbd0e5f88e65d330ee925c56e4afeb
SKOT <skotskip@gmail.com>
2023-08-30 03:24:37 +0700

    finished?

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,43 @@
 "use client";
-import { Container, Text, Title } from "@mantine/core";
+import { Container, Text, Title, Rating, Group, Textarea, Button, Divider } from "@mantine/core";
 
 export default function FoodReviewPage() {
   return (
     <Container size="600px">
       <Title order={2}>Food Review 🍕</Title>
 
-      <Text align="center" color="dimmed" my="sm">
-        Copyright © 2023 Chayanin Suatap 650610560
+      
+      <Title mt="sm" size="h4">Your rating</Title>
+      <Rating size="lg"></Rating>
+
+      <Textarea placeholder="Do you enjoy eating?" label="Your review" mt="xs" minRows={3}></Textarea>
+
+      <Button mt="xs" color="orange">Submit Review</Button>
+
+      <Divider my="sm" size="xs"/>
+
+      <Group position="center">
+        <Title align="center" size="h4">Elon Musk</Title>
+        <Rating readOnly value={5}/>
+        </Group>
+      <Text align="center" color="dimmed">
+        Best pizza in this world. I give you X score.
+      </Text>
+
+      <Divider my="sm" size="xs"/>
+
+      <Group position="center">
+        <Title align="center" size="h4">Mark Zuck</Title>
+        <Rating readOnly value={4}/>
+        </Group>
+      <Text align="center" color="dimmed">
+        My favourite part is pepperoni
+      </Text>
+
+      <Pegination total={20} color="orange" position="center" mt="md"/>
+      
+      <Text align="center" color="dimmed" my="xs">
+        Copyright © 2023 Korawit  Piboon 650612076
       </Text>
     </Container>
   );
